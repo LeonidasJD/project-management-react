fae9b652f916c3e750249a1e307eef0e99fd1050
LeonidasJD <milandjordjevic633@gmail.com>
2024-02-29 22:29:05 +0100

    azurirane komponente

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,18 +1,62 @@
+import {useRef, useState} from 'react';
 import Input from "./Input";
+import Modal from './Modal';
 
-export default function NewProject(){
+export default function NewProject({onAdd}){
+    
+
+   
+
+    const cancelStyle= "text-stone-800 hover:text-stone-950";
+    const saveStle = "px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950";
+
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+    const modal = useRef();
+   
+
+    function onHandleSave(){//izvrsava se kada sacuvamo projekat
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        //validacija ako je bar jedno polje prazno prikazuje se modal sa greskom
+        if(enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === ''){
+             modal.current.showModal();
+             return;//ovo return sluzi da se dalje kod ne izvrsava ako je uslov ispunjen tj ako neko polje ostane prazno dalji kod se ne izvrsava
+        }
+
+        
+        onAdd({Title:enteredTitle,//prop fukncija kojoj kao parametar dodeljujemo objekat vrednosti iz inputa koja ce se pozvati u App komponenti
+            Description:enteredDescription,
+            Due_Date:enteredDueDate});
+    }
+    
+    
+    
     return(
        
          <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
-            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-            <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+            <li><button className={cancelStyle}>Cancel</button></li>
+            <li><button className={saveStle} onClick={()=>onHandleSave()}>Save</button></li>
         </menu>
         <div>
-           <Input label={"Title"}/>
-           <Input label={"Description"} textarea={true}/>
-           <Input label={"Due Date"}/>
+           <Input ref={title} label={"Title"}/>
+           <Input ref={description} label={"Description"} textarea={true}/>
+           <Input type="date" ref={dueDate} label={"Due Date"}/>
         </div>
+        <div>
+            <Modal ref={modal}>
+               <h2>Invalid Input</h2>
+               <p>Ooops...looks like you forgot to enter a value</p>
+               <p>Please make sure you provide a valid value for every filed!</p>
+               
+            </Modal>
+        </div>
+        
+        
     </div>
         
         
--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,15 +1,21 @@
-export default function ProjectSidebar({onStartAddProject}){
+export default function ProjectSidebar({onStartAddProject,myProjects}){
 
     const asideStyle = "w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl";
     const headStyle = "mb-8 font-bold uppercase md:text-xl text-stone-50 ";
     const buttonStyle= "px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100";
+    const projectBtn = "w-full text-left px-2 py-1 rounded my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800"
+
+    let projects = myProjects;//varijabla koja prikazuje preko propa niz objekata jer je prop zapravo azurirano stanje koje cuva objekte projekata
+    console.log(projects);
+
     return(
         <aside className={asideStyle}>
             <h2 className={headStyle}>Your Projects</h2>
             <button onClick={onStartAddProject} className={buttonStyle}>+ Add Project</button>
-            <ul>
-                <li>Test project 1</li>
-                <li>Test project 2</li>
+            <ul className="mt-2"> 
+                {myProjects.map((project)=>(
+                    <li key={project.Id}><button className={projectBtn}>{project.Title}</button></li>//prosli smo kroz niz objekata i ispisali naslove projekata u listu
+                ))}
             </ul>
         </aside>
     );
