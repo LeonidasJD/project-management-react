984f17201854d921ae7999b4f73e93cdc78cebe9
LeonidasJD <milandjordjevic633@gmail.com>
2024-02-29 22:27:56 +0100

    dodato stanje koje cuva niz projekata

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,35 @@ import {useState} from 'react';
 
 function App() {
 const [inputsShow,setInputsShow] = useState(false);
+const [projectStates,setProjectStates] = useState([
+  {Title:'',
+  Description:'',
+  Due_Date:'',
+  Id:''},
+]);
 
 
 function showInputs(){
   setInputsShow(true);
+}
+
+function onHandleAddProject(projecData){ //automatski kada sacuvamo projekat izvrsava se ova funckija koja prihvata objekat podataka iz inputa i kreira novo stanje koje cuva objekat podataka projekta
+  const newProject = {
+    ...projecData,
+    Id:Math.random()
+  };
+
+  // setInputsShow(false);//gasimo inpute nakon sto sacuvamo nov projekat
+  setProjectStates(prevState =>[...prevState,newProject]);
   
 }
 
+console.log(projectStates);
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={()=>showInputs()}/>
-      {inputsShow ? <NewProject /> : <NoProjectSelected onStartAddProject={()=>showInputs()}/>}
+      <ProjectSidebar myProjects={projectStates} onStartAddProject={()=>showInputs()}/>
+      {inputsShow ? <NewProject onAdd={onHandleAddProject}/> : <NoProjectSelected onStartAddProject={()=>showInputs()}/>}
       
       
 
